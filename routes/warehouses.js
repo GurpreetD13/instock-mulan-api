5a31778e092c8aedbeb5ddfa3f721c2536309b6a
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-15 14:12:38 -0400

    created get route for all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+const fetchData = () => { 
+    const warehouses = fs.readFileSync('./data/warehouses.json');
+    return JSON.parse(warehouses);
+}
+
+
 // '/warehouses' route 
 
-
+router.route('/')
+    .get((req, res) => {
+        res.status(200).send(fetchData());
+    })
 
 
 
