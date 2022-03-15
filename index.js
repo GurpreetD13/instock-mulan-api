8f868a7453a0ac8d5083903256d7206d3b5ae211
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-14 22:45:06 -0400

    Set server to listen on port 8080

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// server Listening for requests on port 8080
+app.listen(8080, () => { console.log("InStock server is running on port 8080") });
