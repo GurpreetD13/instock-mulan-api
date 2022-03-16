bebdf748f2dbf483a60401d76c3a2f6123b7e07b
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-16 01:44:55 -0400

    Setup handling of GET request for /inventories route to GET list/array of All Inventory Items for all warehouses

--- routes/inventories.js
+++ routes/inventories.js
@@ -5,12 +5,23 @@ const { v4: uuidv4 } = require('uuid');
 
 // '/inventories' route 
 
+// Function to get All inventory items
+const getAllItems = () => {
+    const allInventoryItems = fs.readFileSync('./data/inventories.json');
+    return JSON.parse(allInventoryItems);
+};
 
 
 
 
 
 
+
+// '/inventories/' route
+router.route('/')
+    .get((req, res) => {
+        res.status(200).json(getAllItems())
+    })
 
 
 
