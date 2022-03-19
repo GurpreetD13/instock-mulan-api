fee4c05bd341d528646a9ac31a179878fc577ac8
Charanpreet Chall <charanpreetchall@gmail.com>
2022-03-19 08:18:28 -0400

    Initial setup for deleting a single warehouse

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const fetchData = () => { 
+const fetchData = () => {
     const warehouses = fs.readFileSync('./data/warehouses.json');
     return JSON.parse(warehouses);
 }
+
 
 
 // '/warehouses' route 
@@ -15,6 +16,20 @@ router.route('/')
     .get((req, res) => {
         res.status(200).send(fetchData());
     })
+
+
+
+    
+// router.delete('/:id', (req, res) => {
+
+//         const warehouses=warehouses.filter(warehouse => warehouse.id === req.params.id);
+
+//         warehouses.splice()
+
+//         res.status(202).json({
+//             message: "Warehouse successfully deleted"
+//         })
+//     })
 
 
 
