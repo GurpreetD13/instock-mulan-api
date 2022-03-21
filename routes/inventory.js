5f11a96bc4ba78436d76b269e1b7dba15a1ef55f
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-20 20:19:55 -0400

    formatted spacing

--- routes/inventory.js
+++ routes/inventory.js
@@ -19,62 +19,62 @@ const writeInventoryData = (inventoryData) => {
 // '/inventories/' route
 router
   .route("/")
-    .get((req, res) => {
-        res.status(200).json(getAllItems());
-    })
-    .post((req, res) => {
-        const warehouses = getAllWarehouses();
-        const inventory = getAllItems();
-        if (
-        !req.body.id ||
-        !itemWarehouse ||
-        !req.body.itemName ||
-        !req.body.itemDescription ||
-        !req.body.itemCategory
-        ) {
-        res.status(204).send("Not enought form data");
-        } else {
-        const newItem = {
-            id: uuidv4(),
-            warehouseID: warehouses.find(
-            (warehouse) => warehouse.name === req.body.itemWarehouse
-            ).id,
-            warehouseName: req.body.itemWarehouse,
-            itemName: req.body.itemName,
-            description: req.body.itemDescription,
-            category: req.body.itemCategory,
-            status:
-            req.body.itemIsAvailable === "in-stock" ? "In Stock" : "Out of Stock",
-            quantity: req.body.itemQuantity,
-        };
-        inventory.push(newItem);
-        writeInventoryData(inventory);
-        res.status(201).send("Success");
-        }
-    })
-    .put((req, res) => {
-        const warehouses = getAllWarehouses();
-        const inventory = getAllItems();
-        const currentItemIndex = inventory.findIndex(item => item.id === req.body.itemId)
+  .get((req, res) => {
+    res.status(200).json(getAllItems());
+  })
+  .post((req, res) => {
+    const warehouses = getAllWarehouses();
+    const inventory = getAllItems();
+    if (
+      !req.body.id ||
+      !itemWarehouse ||
+      !req.body.itemName ||
+      !req.body.itemDescription ||
+      !req.body.itemCategory
+    ) {
+      res.status(204).send("Not enought form data");
+    } else {
+      const newItem = {
+        id: uuidv4(),
+        warehouseID: warehouses.find(
+          (warehouse) => warehouse.name === req.body.itemWarehouse
+        ).id,
+        warehouseName: req.body.itemWarehouse,
+        itemName: req.body.itemName,
+        description: req.body.itemDescription,
+        category: req.body.itemCategory,
+        status:
+          req.body.itemIsAvailable === "in-stock" ? "In Stock" : "Out of Stock",
+        quantity: req.body.itemQuantity,
+      };
+      inventory.push(newItem);
+      writeInventoryData(inventory);
+      res.status(201).send("Success");
+    }
+  })
+  .put((req, res) => {
+    const warehouses = getAllWarehouses();
+    const inventory = getAllItems();
+    const currentItemIndex = inventory.findIndex(item => item.id === req.body.itemId)
 
-        if (!req.body.itemWarehouse || !req.body.itemName || !req.body.itemDescription || !req.body.itemCategory) {
-            res.status(404).send('Not enought form data');
-        } else {
+    if (!req.body.itemWarehouse || !req.body.itemName || !req.body.itemDescription || !req.body.itemCategory) {
+      res.status(404).send('Not enought form data');
+    } else {
 
-            const updatedItem = {
-                id: req.body.itemId,
-                warehouseID: warehouses.find(warehouse => warehouse.name === req.body.itemWarehouse).id,
-                warehouseName: req.body.itemWarehouse,
-                itemName: req.body.itemName,
-                description: req.body.itemDescription,
-                category: req.body.itemCategory,
-                status: req.body.itemIsAvailable === 'In Stock' ? 'In Stock' : 'Out of Stock',
-                quantity: Number(req.body.itemQuantity),
-            }
-            inventory[currentItemIndex] = updatedItem;
-            writeInventoryData(inventory);
-        }
-    });
+      const updatedItem = {
+        id: req.body.itemId,
+        warehouseID: warehouses.find(warehouse => warehouse.name === req.body.itemWarehouse).id,
+        warehouseName: req.body.itemWarehouse,
+        itemName: req.body.itemName,
+        description: req.body.itemDescription,
+        category: req.body.itemCategory,
+        status: req.body.itemIsAvailable === 'In Stock' ? 'In Stock' : 'Out of Stock',
+        quantity: Number(req.body.itemQuantity),
+      }
+      inventory[currentItemIndex] = updatedItem;
+      writeInventoryData(inventory);
+    }
+  });
 
 
 router.route("/:id")
