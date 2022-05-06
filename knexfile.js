d171b4e9e9938814f2adf0dd3552d7486ec0f70f
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-05-06 12:15:27 -0400

    added development env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,14 +4,16 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
  const connections = {
-  client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "instock",
-    charset: "utf8",
-    timezone: 'etc'
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: "rootroot",
+      database: "instock",
+      charset: "utf8",
+      timezone: 'etc'
+    },
   },
   production: {
     client: 'mysql',
