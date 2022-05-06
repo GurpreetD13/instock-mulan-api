cab67f681f6cfd6513c0e43a3f9789af266208b3
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-05-06 12:20:29 -0400

    added ENV port variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const PORT = process.env.PORT || 8080;
+
 
 // Middleware:
 app.use(cors());
@@ -14,4 +16,4 @@ const inventoriesRoutes = require('./routes/inventory');
 app.use('/inventory', inventoriesRoutes);
 
 // server Listening for requests on port 8080
-app.listen(8080, () => { console.log("InStock server is running on port 8080") });
+app.listen(PORT, () => { console.log("InStock server is running on port 8080") });
